5ea4a2fedf7392da9fafa598548880965fcb0655
Mert Demirkıran <mert_demirkiran41@hotmail.com>
2026-05-19 16:40:50 +0300

    test

--- Presentation/LibraryProject.WebApi/Controllers/AboutsController.cs
+++ Presentation/LibraryProject.WebApi/Controllers/AboutsController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LibraryProject.WebApi.Controllers
-{
+{ 
 	[Route("api/[controller]")]
 	[ApiController]
 	public class AboutsController : ControllerBase
