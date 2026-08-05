fb58e308995aa42a529ac7b9b85e716dadb67c34
Mert Demirkıran <mert_demirkiran41@hotmail.com>
2026-08-05 17:12:03 +0300

    Added theme in the project

---
+++ Frontends/LibraryProject.WebUI/Controllers/UILayoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryProject.WebUI.Controllers
+{
+	public class UILayoutController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
